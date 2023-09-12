090d08a94f861a4bd17fd61dbf7182c85bddfc74
sainath <sainathp@cogncise.com>
2023-09-12 15:51:12 +0530

    ll

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -15,84 +15,39 @@ const Login = () => {
 
   return (
     <div className=" d-flex align-items-center justify-content-center  h  "  >
-     
+     <div className=" p-4 border1 d-flex align-items-center justify-content-center" style={{height:720,width:602,backgroundColor:"#FFFFFF"}}>
+
+
+     <form>
+     <img src="./natureshealthcare 1.png"/>
+
+  <div class="form-group" style={{marginBottom:33}}>
+    <label for="exampleInputEmail1 " className="font">Username</label>
+    <input style={{height:50,width:460,borderRadius:10,backgroundColor:"#D9D9D9"}} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group " style={{marginBottom:33}}>
+    <label for="exampleInputPassword1"  className="font">Password</label>
+    <input style={{height:50,width:460,borderRadius:10,backgroundColor:"#D9D9D9"}} type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+  </div>
+  <div className="d-flex flex-row inter">
+  <a style={{color:"#074DFF"}}>Forgot Password?</a>
+
+  <div class="form-check" style={{marginBottom:33,marginLeft:33}}>
+    <input type="checkbox" style={{backgroundColor:"#D9D9D9"}} class="form-check-input" id="exampleCheck1"/>
+    <label class="form-check-label" for="exampleCheck1" >Check me out</label>
+  </div>
+  </div>
+  <div className="d-flex align-items-center justify-content-center " >
+  <button className="d-flex align-items-center justify-content-center text-center" style={{ height:60 ,width:210 ,borderRadius:18,backgroundColor:"#6AE531"}} type="submit">
+    <div   className="font" style={{fontWeight:700,fontSize:36,color:"#FFFFFF"}}> Login</div>
+  </button>
+  </div>
+</form>
+     </div>
           
 
-      <div className=" container h-100  d-flex align-items-center justify-content-center  h-100 " style={{backgroundColor:"whitesmoke"}} >
-        <div className="row-sm-12    " style={{height:896,borderStyle:"solid",borderWidth:3,borderRadius:20,padding:"1rem 4rem"}}>
-            <div className="d-flex justify-content-center ">
-            <img style={{height:180,width:172}} src="./logo.png"></img>
-            </div>
-          <form >
-            <div className="mb-5 flex-col  align-items-center justify-content-center text-center">
-            {/* <h2 className="text"> */}
-            {/* <div onClick={()=>{ */}
-      {/* st.setVal("Register") */}
-    {/* }}> */}
-    {/* <BearCounter/> */}
-    {/* {st.bears} */}
-
-    {/* </div> */}
-
-
-    {/* <div onClick={()=>{ */}
-      {/* st.setData("updating it") */}
-    {/* }}> */}
-    {/* <BearCounter/> */}
-    {/* {st.data} */}
-
-    {/* </div> */}
-
-
-
-              {/* </h2> */}
-            <small>Please enter your login information</small>
-            </div>
-            <div className="form-group ">
-              <label for="exampleInputEmail1" className="mb-3">Username</label>
-              <input
-                type="email"
-                className="form-control mb-5 text_input border"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Enter email"
-              />
-              
-            </div>
-            <div className="form-group">
-              <label for="exampleInputPassword1 mb-3">Password</label>
-              <input
-                type="password"
-                className="form-control mb-5  text_input border"
-                id="exampleInputPassword1"
-                placeholder="Password"
-              />
-            </div>
-            <small id="emailHelp" className=" form-text text-muted mb-3">
-              <u>Forgot password ?</u>
-              </small>
-              <div className="d-flex  align-items-center justify-content-center mb-3">
-            <button type="submit"  className="btn  w-100  border" style={{backgroundColor:"black",color:"white",fontWeight:"bold"}}>
-                LOGIN
-            </button>
-        
-            </div>
-            <div className="form-check">
-              
-              
-            </div>
-        
-            {/* <div className="d-flex justify-content-center"> */}
-
-{/* <small><h7>Don't have an account? </h7>
-<a href="url">
-    <u> Sign Up</u> </a>
-    </small> */}
-{/* </div> */}
-      
-          </form>
-        </div>
-      </div>
+     
       </div>
   );
 };

d476af7ef97513b6bc4d9752fd126c135ede50e3
sainath <sainathp@cogncise.com>
2023-09-13 11:18:31 +0530

    ll

--- src/Screens/Menu.js
+++ src/Screens/Menu.js
@@ -1,99 +1,220 @@
 import React from "react";
-import "../App.css"
-const Menu=()=> {
+import "../App.css";
+const Menu = () => {
   return (
-    <div className="menu"> 
-    <div className="h2 mt-3" style={{width:389}}>
+    <div>
+      <div className="h2 mt-3 " style={{ width: 270 }}>
+        <div className="h-75">
+          <div>
+            <div class="dropdown show d-flex flex-row p-3 border m-1">
+              <img
+                src="./Dashboard/ic_sharp-dashboard.png"
+                style={{ width: 35, height: 35 }}
+              />
+              <a
+                style={{ width: 250 }}
+                class="btn "
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dashboard{" "}
+              </a>
 
-          <div className="h-75">
-          <div  style={{marginTop:10}}>
-        <div class="dropdown show d-flex flex-row p-5  m-1">
-        <img src="./Dashboard/ic_sharp-dashboard.png"/>
+              {/* <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div> */}
+            </div>
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <div class="dropdown show d-flex flex-row p-3 border m-1">
+              <img
+                src="./Dashboard/Group.png"
+                style={{ width: 35, height: 35 }}
+              />
 
-  <a    class="btn" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-Dashboard          <img src="./Dashboard/drop.png"/>
-</a>
+              <a
+                style={{ width: 250 }}
+                class="btn  dropdown-toggle"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Customers{" "}
+              </a>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="dropdownMenuLink"
+                style={{ width: 200 }}
+              >
+                <a class="dropdown-item" href="/customers">
+                  Customer List
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <div class="dropdown show d-flex flex-row p-3 border m-1">
+              <img
+                src="./Dashboard/eos-icons_cluster-management.png"
+                style={{ width: 35, height: 35 }}
+              />
 
+              <a
+                style={{ width: 250 }}
+                class="btn  dropdown-toggle"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                User Management
+              </a>
 
-</div>
-        </div>
-        <div  style={{marginTop:10}}>
-        <div class="dropdown show d-flex flex-row p-5  m-1">
-        <img src="./Dashboard/Group.png"/>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="dropdownMenuLink"
+                style={{ width: 200 }}
+              >
+                <a class="dropdown-item" href="#">
+                  Add Agent
+                </a>
+                <a class="dropdown-item" href="#">
+                  List of Users
+                </a>
+                <a class="dropdown-item" href="#">
+                  Agent History
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <div class="dropdown show d-flex flex-row p-3 border m-1">
+              <img
+                src="./Dashboard/Vector(9).png"
+                style={{ width: 35, height: 35 }}
+              />
+              <a
+                style={{ width: 250 }}
+                class="btn dropdown-toggle"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Orders
+              </a>
 
-  <a   style={{width:250}} class="btn  " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-Customers   <img src="./Dashboard/drop.png"/> </a>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="dropdownMenuLink"
+                style={{ width: 200 }}
+              >
+                <a class="dropdown-item" href="#">
+                  Order History
+                </a>
+                <a class="dropdown-item" href="#">
+                  To be Dispatch
+                </a>
+                <a class="dropdown-item" href="#">
+                  Upcomming Orders
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <div class="dropdown show d-flex flex-row p-3 border m-1 ">
+              <img
+                src="./Dashboard/iconoir_reports.png"
+                style={{ width: 35, height: 35 }}
+              />
+              <a
+                style={{ width: 250 }}
+                class="btn dropdown-toggle"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Reports{" "}
+              </a>
 
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <a class="dropdown-item" href="/customers">Customer List</a>
-    
-  </div>
-</div>
-        </div>
-        <div  style={{marginTop:10}}> 
-        <div class="dropdown show d-flex flex-row p-5  m-1">
-        <img src="./Dashboard/eos-icons_cluster-management.png"/>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="dropdownMenuLink"
+                style={{ width: 200 }}
+              >
+                <a class="dropdown-item" href="#">
+                  Sales Report{" "}
+                </a>
+                <a class="dropdown-item" href="#">
+                  Customer Report{" "}
+                </a>
+                <a class="dropdown-item" href="#">
+                  Product Report{" "}
+                </a>
+                <a class="dropdown-item" href="#">
+                  Financial Report{" "}
+                </a>
+                <a class="dropdown-item" href="#">
+                  Inventory Report{" "}
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <div class="dropdown show d-flex flex-row p-3 border m-1 ">
+              <img
+                src="./Dashboard/gridicons_refund.png"
+                style={{ width: 35, height: 35 }}
+              />
+              <h1
+                style={{ width: 250 }}
+                class="btn dropdown-toggle"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Refunds
+              </h1>
 
-  <a  style={{width:250}} class="btn " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-User Management <img src="./Dashboard/drop.png"/>
-  </a>
-
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <a class="dropdown-item" href="#">Add Agent</a>
-    <a class="dropdown-item" href="#">List of Users</a>
-    <a class="dropdown-item" href="#">Agent History</a>
-  </div>
-</div>
-        </div>
-        <div  style={{marginTop:10}}>
-        <div class="dropdown show d-flex flex-row p-5  m-1">
-          <img src="./Dashboard/Vector(9).png"/>
-  <a   style={{width:250}} class="btn " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-Orders <img src="./Dashboard/drop.png"/>
-  </a>
-
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <a class="dropdown-item" href="#">Order History</a>
-    <a class="dropdown-item" href="#">To be Dispatch</a>
-    <a class="dropdown-item" href="#">Upcomming Orders</a>
-  </div>
-</div>
-        </div>
-        <div  style={{marginTop:10}}>
-        <div class="dropdown show d-flex flex-row p-5  m-1 ">
-          <img src="./Dashboard/iconoir_reports.png"/>
-  <a   style={{width:250}}class="btn" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-
-Reports <img src="./Dashboard/drop.png"/> </a>
-
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <a class="dropdown-item" href="#">Sales Report </a>
-    <a class="dropdown-item" href="#">Customer Report </a>
-    <a class="dropdown-item" href="#">Product Report </a>
-    <a class="dropdown-item" href="#">Financial Report  </a>
-    <a class="dropdown-item" href="#">Inventory Report  </a>
-
-  </div>
-</div>
-        </div>
-        <div  style={{marginTop:10}}>
-        <div class="dropdown show d-flex flex-row p-5  m-1 ">
-          <img   src="./Dashboard/gridicons_refund.png"/>
-  <a  style={{width:250}} class="btn  " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-
-Refunds <img src="./Dashboard/drop.png"/>
-  </a>
-
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <a class="dropdown-item" href="#">Refund Tracking</a>
-    <a class="dropdown-item" href="#">Refund Analytics</a>
-    <a class="dropdown-item" href="#">Refund Policy</a>
-  </div>
-</div>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="dropdownMenuLink"
+                style={{ width: 200 }}
+              >
+                <a class="dropdown-item" href="#">
+                  Refund Tracking
+                </a>
+                <a class="dropdown-item" href="#">
+                  Refund Analytics
+                </a>
+                <a class="dropdown-item" href="#">
+                  Refund Policy
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    </div>
   );
-}
-export default Menu
+};
+export default Menu;
--- src/Screens/Verify.js
+++ src/Screens/Verify.js
@@ -3,48 +3,28 @@ import "../App.css"
 const Verify = () => {
   return (
     <div
-      className="h  d-flex align-items-center justify-content-between"
+      className="h  d-flex align-items-center justify-content-center"
       style={{ backgroundColor: "#A4E4B6", }}
     >
       <div
-        className="container w-75"
-        style={{ backgroundColor: "#FFFFFF" ,height:713}}
+        className="container-fluid text-center"
+        style={{ backgroundColor: "#FFFFFF" ,height:713,width:1369}}
       >
-        <div className="row">
-          <div className=" d-flex col justify-content-center m-4 "style={{fontFamily:"inter",fontWeight:900,fontSize:900}}>
-            <h1 >Enter your Verification Code</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className=" d-flex col justify-content-center m-1">
-            <h5>We have sent a verification code </h5>
-          </div>
-        </div>
-        <div className="row">
-          <div className=" d-flex col justify-content-center m-4">
-            <h5> to your email ID</h5>
-          </div>
-        </div>
-        <div className=" d-flex row justify-content-center m-4">
-          <div className="col-sm-1  box m-2">
+        <div>
+          <div style={{marginTop:232,fontWeight:400,fontSize:64}}> 
+          <h1>Enter your Verification Code</h1>
 
           </div>
-          <div className="col-sm-1 box m-2">
+          <div className="row justify-content-center"> 
+          <h9 style={{fontSize:40,fontWeight:400}}>We have sent a verification code to your email ID</h9>
 
           </div>
-          <div className="col-sm-1 box m-2">
+          <div className="row justify-content-center">
+            <h6 style={{fontSize:40,fontWeight:400}}>
+            <a><u style={{color:"#019332"}}>Send code again</u></a>
+            </h6>
 
           </div>
-          <div className="col-sm-1 box m-2">
-
-          </div>
-        </div>
-        <div className="row d-flex justify-content-center m-4">
-            <u style={{color:"#019332",fontFamily:"Montserrat",fontWeight:100,fontSize:25}}>Send code again</u>
-        </div>
-        <div className="row d-flex justify-content-center my-2" >
-        <button type="button " class="btn "  style={{width:200,backgroundColor:"#1CE45F",fontWeight:800,fontSize:23}}>Verify</button>
-
 
         </div>
       </div>
